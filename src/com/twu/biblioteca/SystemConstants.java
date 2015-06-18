71513e86e7696453124287339982ed2b81205162
HarrisonSong <harrisonsong1991@gmail.com>
2015-06-18 11:41:49 +0800

    categorize constans options in System Constant class

--- src/com/twu/biblioteca/SystemConstants.java
+++ src/com/twu/biblioteca/SystemConstants.java
@@ -4,23 +4,34 @@ package com.twu.biblioteca;
  * Created by qiyuesong on 16/6/15.
  */
 public class SystemConstants {
+
+    //Main Menu Options
     public static final String MAIN_MENU_LIST_BOOKS_OPTION = "List Books";
-    public static final String QUIT_OPTION = "quit";
+
+    //User Current Position
     public static final String SYSTEM_POSITION_MAIN_MENU = "main menu";
     public static final String SYSTEM_POSITION_LIST_BOOKS = "list books";
+
+    //System Messages
     public static final String WELCOME_MESSAGE = "Welcome to biblioteca library management system.\nBelow is the operations you can do.";
-    public static final String INVALID_MENU_OPTION = "invalid menu option";
     public static final String INVALID_MENU_OPTION_MESSAGE = "Select a valid option!";
-    public static final String SUCCESSFUL_CHECKOUT = "successful checkout";
     public static final String SUCCESSFUL_CHECKOUT_MESSAGE = "Thank you! Enjoy the book.";
-    public static final String UNSUCCESSFUL_CHECKOUT = "unsuccessful checkout";
     public static final String UNSUCCESSFUL_CHECKOUT_MESSAGE = "That book is not available.";
-    public static final String SUCCESSFUL_RETURN = "successful return";
     public static final String SUCCESSFUL_RETURN_MESSAGE = "Thank you for returning the book.";
-    public static final String UNSUCCESSFUL_RETURN = "unsuccessful return";
     public static final String UNSUCCESSFUL_RETURN_MESSAGE = "That is not a valid book to return.";
-    public static final String INVALID_BOOK_OPTION = "invalid book option";
     public static final String INVALID_BOOK_OPTION_MESSAGE = "Your operation is not available.";
-    public static final String BACK_OPTION = "back";
     public static final String REMINDING_MESSAGE = "Please type in the operation you want to do: ";
+
+    //System Message Types
+    public static final String INVALID_MENU_OPTION = "invalid menu option";
+    public static final String SUCCESSFUL_CHECKOUT = "successful checkout";
+    public static final String UNSUCCESSFUL_CHECKOUT = "unsuccessful checkout";
+    public static final String SUCCESSFUL_RETURN = "successful return";
+    public static final String UNSUCCESSFUL_RETURN = "unsuccessful return";
+    public static final String INVALID_BOOK_OPTION = "invalid book option";
+
+    //System Options
+    public static final String BACK_OPTION = "back";
+    public static final String QUIT_OPTION = "quit";
+
 }
