612c4ad4795b4852ac1e5d34e5f680cde52ce6ac
HarrisonSong <harrisonsong1991@gmail.com>
2015-06-21 14:41:36 +0800

    refactored the code to pass the tests

--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -1,5 +1,6 @@
 package com.twu.biblioteca;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
 /**
@@ -13,10 +14,18 @@ public class Library {
     }
 
     public LinkedList<Book> getBooksList() {
-        return null;
+        return this.booksList;
     }
 
     public LinkedList<Book> getAvailableBooksList(){
-        return null;
+        LinkedList<Book> availableBooksList = new LinkedList<Book>();
+        Iterator<Book> iterator = this.booksList.iterator();
+        while(iterator.hasNext()){
+            Book currentBook = iterator.next();
+            if(!currentBook.getCheckOutStatus()){
+                availableBooksList.addLast(currentBook);
+            }
+        }
+        return availableBooksList;
     }
 }
